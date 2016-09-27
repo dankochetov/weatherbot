de70e8ccc7a07cf4a67a2389e599414a110bb309
kochetovdd <danil.kochetov@gmail.com>
2016-09-27 21:41:26 +0300

    Some testing

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ function receivedMessage(event) {
 		if (!error && response.statusCode == 200) {
 			sendTextMessage(senderID, formResponseMessage(body));
 		} else {
-			console.log(response.status.errorDetails);
+			console.log(response.status);
 		}
 	});
 }
