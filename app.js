5fffede4802286995fc5dffe1e88002ab2427235
kochetovdd <danil.kochetov@gmail.com>
2016-09-28 22:42:50 +0300

    Reworked the code with some ES6 magic

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 const 
 	bodyParser = require('body-parser'),
@@ -7,78 +7,77 @@ const
 	https = require('https'),	
 	request = require('request'),
 	dateFormat = require('dateformat'),
-	defer = require('./defer');
+	defer = require('./defer')
 
-var app = express();
-app.set('port', process.env.PORT || 3000);
-app.set('view engine', 'ejs');
-app.use(bodyParser.json());
-app.use(express.static('public'));
+var app = express()
+app.set('port', process.env.PORT || 3000)
+app.set('view engine', 'ejs')
+app.use(bodyParser.json())
+app.use(express.static('public'))
 
-const VALIDATION_TOKEN = process.env.MESSENGER_VALIDATION_TOKEN || '';
+const VALIDATION_TOKEN = process.env.MESSENGER_VALIDATION_TOKEN || ''
 
-const PAGE_ACCESS_TOKEN = process.env.MESSENGER_PAGE_ACCESS_TOKEN || '';
+const PAGE_ACCESS_TOKEN = process.env.MESSENGER_PAGE_ACCESS_TOKEN || ''
 
-const API_AI_ACCESS_TOKEN = process.env.API_AI_ACCESS_TOKEN || '';
+const API_AI_ACCESS_TOKEN = process.env.API_AI_ACCESS_TOKEN || ''
 
-const WEATHER_API_KEY = process.env.WEATHER_API_KEY || '';
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY || ''
 
-app.get('/webhook', function(req, res) {
+app.get('/webhook', (req, res) => {
 	if (req.query['hub.mode'] === 'subscribe' &&
 		req.query['hub.verify_token'] === VALIDATION_TOKEN) {
-	console.log("Validating webhook");
-	res.status(200).send(req.query['hub.challenge']);
+	console.log("Validating webhook")
+	res.status(200).send(req.query['hub.challenge'])
 	} else {
-	console.error("Failed validation. Make sure the validation tokens match.");
-	res.sendStatus(403);		
+	console.error("Failed validation. Make sure the validation tokens match.")
+	res.sendStatus(403)		
 	}	
-});
+})
 
-app.post('/webhook', function (req, res) {
-	var data = req.body;
+app.post('/webhook', (req, res) => {
+	var data = req.body
 
 	// Make sure this is a page subscription
 	if (data.object == 'page') {
 		// Iterate over each entry
 		// There may be multiple if batched
-		data.entry.forEach(function(pageEntry) {
-			var pageID = pageEntry.id;
-			var timeOfEvent = pageEntry.time;
+		data.entry.forEach((pageEntry) => {
+			var pageID = pageEntry.id
+			var timeOfEvent = pageEntry.time
 
 			// Iterate over each messaging event
-			pageEntry.messaging.forEach(function(messagingEvent) {
+			pageEntry.messaging.forEach((messagingEvent) => {
 			if (messagingEvent.message) {
-				receivedMessage(messagingEvent);
+				receivedMessage(messagingEvent)
 			}
 			else {
-				console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+				console.log(`Webhook received unknown messagingEvent: ${messagingEvent}`)
 			}
-			});
-		});
-		res.sendStatus(200);
+			})
+		})
+		res.sendStatus(200)
 	}
-});
+})
 
 function receivedMessage(event) {
-	var senderID = event.sender.id;
-	var recipientID = event.recipient.id;
-	var timeOfMessage = event.timestamp;
-	var message = event.message;
+	var senderID = event.sender.id
+	var recipientID = event.recipient.id
+	var timeOfMessage = event.timestamp
+	var message = event.message
 
-	console.log("Received message for user %d and page %d at %d with message:", 
-	senderID, recipientID, timeOfMessage);
-	console.log(JSON.stringify(message));
+	console.log(`Received message for user ${senderID} and page ${recipientID} at ${timeOfMessage} with message:`)
+	console.log(JSON.stringify(message))
 
-	sendReadReceipt(senderID);
-	sendTypingOn(senderID);
+	sendReadReceipt(senderID)
+	sendTypingOn(senderID)
 
-	(function(senderID) {
+	((senderID) => {
 		request(
 		{
 			url: 'https://api.api.ai/v1/query',
 			headers: {
-				'Authorization': 'Bearer ' + API_AI_ACCESS_TOKEN,
-				'Content-Type': 'application/json; charset=utf-8'
+				'Authorization': `Bearer ${API_AI_ACCESS_TOKEN}`,
+				'Content-Type': 'application/json charset=utf-8'
 			},
 			method: 'GET',
 			qs: {
@@ -87,82 +86,82 @@ function receivedMessage(event) {
 				sessionId: '1234567890',
 				lang: 'en'
 			}
-		}, function (error, response, body) {
+		}, (error, response, body) => {
 			if (!error && response.statusCode == 200) {
-				var queryParams = getQueryParams(JSON.parse(body));
-				sendTextMessage(senderID, formResponseMessage(queryParams));
+				var queryParams = getQueryParams(JSON.parse(body))
+				sendTextMessage(senderID, formResponseMessage(queryParams))
 				if (!queryParams.fallback) {
-					sendTypingOn(senderID);
-					getForecast(queryParams).then(function(forecast) {
-						console.log('promise');
-						sendTextMessage(senderID, forecast);
-					});
+					sendTypingOn(senderID)
+					getForecast(queryParams).then((forecast) => {
+						console.log('promise')
+						sendTextMessage(senderID, forecast)
+					})
 				}
 			}
-		});
-	})(senderID);
+		})
+	})(senderID)
 }
 
 function getQueryParams(body) {
-	console.log(body.result.metadata.intentName);
+	console.log(body.result.metadata.intentName)
 	if (body.result.metadata.intentName != 'show weather')
 		return {
 			fallback: true,
 			text: body.result.fulfillment.speech
-		};
+		}
 
-	var city = body.result.parameters.address.city;
-	var state = body.result.parameters.address.state;
-	var date = body.result.parameters.date;
-	var time = body.result.parameters.time;
-	var hasCity = city != null && city != "";
-	var hasDate = date != null && date != "";
-	var hasTime = time != null && time != "";
-	var hasState = state != null && state != "";
+	var city = body.result.parameters.address.city
+	var state = body.result.parameters.address.state
+	var date = body.result.parameters.date
+	var time = body.result.parameters.time
+	var hasCity = city != null && city != ""
+	var hasDate = date != null && date != ""
+	var hasTime = time != null && time != ""
+	var hasState = state != null && state != ""
 	if (!hasCity) {
 		if (hasState)
-			city = state;
+			city = state
 		else
-			city = 'your location';
+			city = 'your location'
 	}
 	else if (hasState) {
-		city += ', ' + state;
+		city += ', ' + state
 	}
 	if (!hasDate)
-		date = dateFormat(new Date(body.timestamp), 'isoDate');
+		date = dateFormat(new Date(body.timestamp), 'isoDate')
 	if (!hasTime) {
 		if (hasDate)
-			time = '00:00:00';
+			time = '00:00:00'
 		else
-			time = dateFormat(new Date(body.timestamp), 'isoTime');
+			time = dateFormat(new Date(body.timestamp), 'isoTime')
 	}
 
 	return {
 		fallback: false,
-		time: time,
-		hasTime: hasTime,
-		date: date,
-		hasDate: hasDate,
-		city: city,
-		hasCity: hasCity,
-		state: state,
-		hasState: hasState
-	};
+		time,
+		hasTime,
+		date,
+		hasDate,
+		city,
+		hasCity,
+		state,
+		hasState
+	}
 }
 
 function formResponseMessage(params) {
 	if (params.fallback)
-		return params.text;
-	var result = 'You requested a weather forecast in ' + params.city + ' for ' + params.date + ' at ' + params.time + '.';
-	return result;
+		return params.text
+	var result = `You requested a weather forecast in ${params.city} for ${params.date} ${params.time}.`
+	return result
 }
 
 function getForecast(params) {
-	var result = defer();
+	var result = defer()
 
 	if (!params.hasCity) {
-		result.resolve('Cannot determine weather forecast in this location. Please specify the correct city name. Also, it may be that you specified the city I just don\'t know.');
-		return result;
+		result.resolve(`Cannot determine weather forecast in this location. Please specify the correct city name. Also, it may be that you specified the city I just don't know.`)
+		return result
 	}
 
 	request(
@@ -173,43 +172,44 @@ function getForecast(params) {
 			APPID: WEATHER_API_KEY,
 			units: 'metric'
 		}
-	}, function(err, response, body) {
-		var allweather = JSON.parse(body);
+	}, (err, response, body) => {
+		var allweather = JSON.parse(body)
 
 		if (!allweather.list) {
-			console.log(allweather);
-			result.resolve('Cannot determine weather forecast in this location. Please try something else.');
-			return result;
+			console.log(allweather)
+			result.resolve('Cannot determine weather forecast in this location. Please try something else.')
+			return result
 		}
 
 		// Picking the closest forecast available from the list
 		// The weather API gives me the forecast for 5 days maximum
-		var closestForecastInd = 0;
-		var closestForecastDt = 1e20;	
-		var timeRequested = new Date(params.date + ' ' + params.time);
+		var closestForecastInd = 0
+		var closestForecastDt = 1e20	
+		var timeRequested = new Date(params.date + ' ' + params.time)
 		for (var curForecastInd in allweather.list) {
-			var curDt = Math.abs((new Date(allweather.list[curForecastInd].dt_txt)) - timeRequested);
+			var curDt = Math.abs((new Date(allweather.list[curForecastInd].dt_txt)) - timeRequested)
 			if (curDt < closestForecastDt) {
-				closestForecastInd = curForecastInd;
-				closestForecastDt = curDt;
+				closestForecastInd = curForecastInd
+				closestForecastDt = curDt
 			}
 		}
 
-		var weather = allweather.list[closestForecastInd];
-		var weatherDate = new Date(allweather.list[closestForecastInd].dt_txt);
+		var weather = allweather.list[closestForecastInd]
+		var weatherDate = new Date(allweather.list[closestForecastInd].dt_txt)
 
-		var newLine = String.fromCharCode(10);
+		var newLine = String.fromCharCode(10)
 
-		var str = 'Showing closest weather available: ' + dateFormat(weatherDate, 'isoDate') + ' ' + dateFormat(weatherDate, 'isoTime') + newLine + newLine;
-		str += 'Weather type: ' + weather.weather[0].description + newLine;
-		str += 'Temperature: ' + weather.main.temp + ' °C' + newLine;
-		str += 'Humidity: ' + weather.main.humidity + '%' + newLine;
-		str += 'Wind speed: ' + weather.wind.speed + ' m/s' + newLine;
-		str += 'Cloudiness: ' + weather.clouds.all + '%';
-		result.resolve(str);
-	});
+		var str =
+		`Showing closest weather available: ${dateFormat(weatherDate, 'isoDate') + dateFormat(weatherDate, 'isoTime') + newLine + newLine}
+		Weather type: ${weather.weather[0].description + newLine}
+		Temperature: ${weather.main.temp} °C ${newLine}
+		Humidity: ${weather.main.humidity}% ${newLine}
+		Wind speed: ${weather.wind.speed} m/s ${newLine}
+		Cloudiness: ${weather.clouds.all}%`
+		result.resolve(str)
+	})
 
-	return result;
+	return result
 }
 
 /*
@@ -225,9 +225,9 @@ function sendTextMessage(recipientId, messageText) {
 		text: messageText,
 		metadata: "DEVELOPER_DEFINED_METADATA"
 	}
-	};
+	}
 
-	callSendAPI(messageData);
+	callSendAPI(messageData)
 }
 
 /*
@@ -235,16 +235,16 @@ function sendTextMessage(recipientId, messageText) {
  *
  */
 function sendReadReceipt(recipientId) {
-	console.log("Sending a read receipt to mark message as seen");
+	console.log("Sending a read receipt to mark message as seen")
 
 	var messageData = {
 	recipient: {
 		id: recipientId
 	},
 	sender_action: "mark_seen"
-	};
+	}
 
-	callSendAPI(messageData);
+	callSendAPI(messageData)
 }
 
 /*
@@ -252,16 +252,16 @@ function sendReadReceipt(recipientId) {
  *
  */
 function sendTypingOn(recipientId) {
-	console.log("Turning typing indicator on");
+	console.log("Turning typing indicator on")
 
 	var messageData = {
 	recipient: {
 		id: recipientId
 	},
 	sender_action: "typing_on"
-	};
+	}
 
-	callSendAPI(messageData);
+	callSendAPI(messageData)
 }
 
 /*
@@ -269,16 +269,16 @@ function sendTypingOn(recipientId) {
  *
  */
 function sendTypingOff(recipientId) {
-	console.log("Turning typing indicator off");
+	console.log("Turning typing indicator off")
 
 	var messageData = {
 	recipient: {
 		id: recipientId
 	},
 	sender_action: "typing_off"
-	};
+	}
 
-	callSendAPI(messageData);
+	callSendAPI(messageData)
 }
 
 /*
@@ -286,16 +286,16 @@ function sendTypingOff(recipientId) {
  *
  */
 function sendReadReceipt(recipientId) {
-	console.log("Sending a read receipt to mark message as seen");
+	console.log("Sending a read receipt to mark message as seen")
 
 	var messageData = {
 		recipient: {
 			id: recipientId
 		},
 		sender_action: "mark_seen"
-	};
+	}
 
-	callSendAPI(messageData);
+	callSendAPI(messageData)
 }
 
 /*
@@ -304,34 +304,32 @@ function sendReadReceipt(recipientId) {
  *
  */
 function callSendAPI(messageData) {
-	console.log(messageData);
+	console.log(messageData)
 	request(
 	{
 		uri: 'https://graph.facebook.com/v2.6/me/messages',
 		qs: { access_token: PAGE_ACCESS_TOKEN },
 		method: 'POST',
 		json: messageData
-	}, function (error, response, body) {
+	}, (error, response, body) => {
 		if (!error && response.statusCode == 200) {
-			var recipientId = body.recipient_id;
-			var messageId = body.message_id;
+			var recipientId = body.recipient_id
+			var messageId = body.message_id
 
 			if (messageId) {
-			console.log("Successfully sent message with id %s to recipient %s", 
-				messageId, recipientId);
+				console.log(`Successfully sent message with id ${messageId} to recipient ${recipientId}`)
 			} else {
-			console.log("Successfully called Send API for recipient %s", 
-			recipientId);
+				console.log(`Successfully called Send API for recipient ${recipientId}`)
 			}
 		} else {
-			console.log(error || response.statusCode);
+			console.log(error || response.statusCode)
 		}
-	});
+	})
 }
 
 // Start server
-app.listen(app.get('port'), function() {
-	console.log('Node app is running on port', app.get('port'));
-});
+app.listen(app.get('port'), () => {
+	console.log(`Node app is running on port ${app.get('port')}`)
+})
 
-module.exports = app;
+module.exports = app
